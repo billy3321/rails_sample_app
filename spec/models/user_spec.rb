5f721c935da7370a8e5714f40aad3ba24623e483
billy3321 <billy3321@gmail.com>
2014-09-23 21:16:13 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -48,16 +50,16 @@ describe User do
         expect(@user).to be_valid
       end
     end
-   end
-   describe "when email address is already taken" do
+  end
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.save
     end
 
-     it { should_not be_valid }
-   end
-   describe "when email address is already taken" do
+    it { should_not be_valid }
+  end
+  describe "when email address is already taken" do
     before do
       user_with_same_email = @user.dup
       user_with_same_email.email = @user.email.upcase
@@ -95,18 +97,23 @@ describe User do
   end
 
   describe "return value of authenticate method" do
-  before { @user.save }
-  let(:found_user) { User.find_by(email: @user.email) }
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
 
-  describe "with valid password" do
-    it { should eq found_user.authenticate(@user.password) }
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
   end
 
-  describe "with invalid password" do
-    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
-
-    it { should_not eq user_for_invalid_password }
-    specify { expect(user_for_invalid_password).to be_false }
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
-end
 end
